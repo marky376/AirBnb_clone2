53b06d66c9961977f387c5caaa4404c6652d8d18
marky376 <mmutuamark@gmail.com>
2024-02-07 22:23:53 +0300

    base_model.py

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+class amenity(BaseModel):
+    """Amenity class that inherits from BaseModel"""
+    name = ""
+# Path: models/amenity.py
+
+    {   
+        'name': 'Amenity'
+        
+
+
+
+    }
